3314b7a5fe4bbf2c9beb9bed4d71c444b86ed9a6
zelad <rodrigue.beck@gmail.com>
2016-01-25 18:50:10 +0100

    Fin de session, ajout du constructeur de la partie socket, côté python

--- SocketPthon/controller/main.py
+++ SocketPthon/controller/main.py
@@ -4,6 +4,13 @@ Created on 15 janv. 2016
 @author: Kiki
 @note: inspiration socket ici: https://openclassrooms.com/courses/apprenez-a-programmer-en-python/le-reseau
 '''
+import socket
 
+class TemplateSocket:
+    
+    def __init__(self):
+        self.mainSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        '''arrivé ici: https://openclassrooms.com/courses/apprenez-a-programmer-en-python/le-reseau#/id/r-2235028'''
+        
 if __name__ == '__main__':
     pass
