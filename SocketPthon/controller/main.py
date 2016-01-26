13d7b5f4eed4392797a9c53c79256a177f2d4616
rbeck <rbeck@assystem.com>
2016-01-26 17:46:21 +0100

    Essai de "handler" WebSocket 	KO: socket et websocket sont différents!

--- SocketPthon/controller/main.py
+++ SocketPthon/controller/main.py
@@ -1,16 +1,56 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
 '''
 Created on 15 janv. 2016
 
 @author: Kiki
 @note: inspiration socket ici: https://openclassrooms.com/courses/apprenez-a-programmer-en-python/le-reseau
 '''
+
 import socket
 
-class TemplateSocket:
-    
-    def __init__(self):
-        self.mainSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        '''arrivé ici: https://openclassrooms.com/courses/apprenez-a-programmer-en-python/le-reseau#/id/r-2235028'''
-        
-if __name__ == '__main__':
-    pass
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(("", 9999))
+sock.listen(5)
+ 
+handshake = '\
+HTTP/1.1 101 Web Socket Protocol Handshake\r\n\
+Upgrade: WebSocket\r\n\
+Connection: Upgrade\r\n\
+WebSocket-Origin: http://localhost:8888\r\n\
+WebSocket-Location: ws://localhost:9999/\r\n\r\n\
+'
+handshaken = False
+ 
+print "TCPServer Waiting for client on port 9999"
+ 
+import sys
+ 
+data = ''
+header = ''
+ 
+client, address = sock.accept()
+while True:
+    if handshaken == False:
+        header += client.recv(16)
+        if header.find('\r\n\r\n') != -1:
+            data = header.split('\r\n\r\n', 1)[1]
+            handshaken = True
+            client.send(handshake)
+    else:
+            tmp = client.recv(128)
+            data += tmp;
+ 
+            validated = []
+ 
+            msgs = data.split('\xff')
+            data = msgs.pop()
+ 
+            for msg in msgs:
+                if msg[0] == '\x00':
+                    validated.append(msg[1:])
+ 
+            for v in validated:
+                print v
+                client.send('\x00' + v + '\xff')
+                
